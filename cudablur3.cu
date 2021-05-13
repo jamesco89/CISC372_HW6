80ab4ad41b19ef7732155d5ff952bd3e3ee00b50
James Cooper <jamesco@br013.ib.bridges2.psc.edu>
2021-05-13 15:28:50 -0400

    deleted output

--- cudablur3.cu
+++ cudablur3.cu
@@ -164,10 +164,7 @@ int main(int argc,char** argv){
     
     //Wait for GPU to finish before accessing on host
     cudaDeviceSynchronize();
-    
-    // Allocate Unified Memory -- accessible from CPU or GPU
-    cudaMallocManaged(&img, sizeof(uint8_t)*pWidth*height);    
-
+        
     numBlocks = (height + blockSize - 1) / blockSize;
     
     // Excecuting a computeRow kernel
@@ -182,26 +179,24 @@ int main(int argc,char** argv){
     t2 = clock();
   
     hostDest = (float*)malloc(sizeof(float)*pWidth*height);
-    cudaMemcpy(hostDest, dest, sizeof(float)*pWidth*height, cudaMemcpyDeviceToHost);
-    
-    cudaFree(dest);	 
+    cudaMemcpy(hostDest, dest, sizeof(float)*pWidth*height, cudaMemcpyDeviceToHost);	 
     
     // Now back to int8 so we can save it
-    //img = (uint8_t*)malloc(sizeof(uint8_t)*pWidth*height);
+    img = (uint8_t*)malloc(sizeof(uint8_t)*pWidth*height);
     for (int i = 0; i < pWidth*height; i++){
         img[i] = (uint8_t)hostDest[i];
     	}
     
-    // free(hostDest);
-    
     // Display the result of the image after applying a gauss blur method
     stbi_write_png("output.png", width, height, bpp, img, bpp*width);
-    
-     cudaFree(img);
     
     // Show the time to complete the image after processing with the radius we desired
     printf("Blur with radius %d complete in %f seconds\n", radius, (t2 - t1) / CLOCKS_PER_SEC);
     
-    cudaFree(destImg);
+    // Free memory
+    cudaFree(dest);
+    free(img);
+    free(hostDest);
+    
 
 }
